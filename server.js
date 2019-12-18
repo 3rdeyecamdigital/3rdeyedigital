673b85aca8910c1c480504cccc4f941e82dc9c78
auzundere <auzundere@yahoo.com>
2019-12-18 01:40:21 -0600

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.get('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
 	res.render('index');
+	console.log(req.originalUrl);
 });
 
 app.post('*', (req,res) => {
