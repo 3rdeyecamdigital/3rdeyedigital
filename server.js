f5f74b32f302220f1a86016dfd521069ff05da68
auzundere <auzundere@yahoo.com>
2019-12-18 01:13:49 -0600

    Update server.js

--- server.js
+++ server.js
@@ -22,21 +22,46 @@
 // 	console.log('Our app is running on http://localhost:' + port);
 // });
 
-const express = require('express')
-const app = express()
-const util = require('util')
-const port = process.env.PORT || 8080;
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
-// const port = 8080;
+
+// const express = require('express')
+// const app = express()
+// const util = require('util')
+// const port = process.env.PORT || 8080;
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static('client/build'));
+// }
+// // const port = 8080;
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.render('index');
+	console.log(req.ip);
+  	console.log(req.originalUrl);
+  	console.log(util.inspect(req.body, {showHidden: false, depth: null}));
+  	res.send("OK")
+});
+
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+// app.get('*',(req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
 // app.get('*', (req, res) => {
 //   console.log(req.ip);
 //   console.log(req.originalUrl);
