8aa3236d3e94a8434e80c6b1592fe6e4ad4b95a2
auzundere <auzundere@yahoo.com>
2019-12-18 01:56:55 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 const util = require('util')
-const bodyParser = require('body-parser');
-app.use(bodyParser);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
